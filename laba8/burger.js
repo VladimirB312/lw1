d029be81e2075c2969dd5c74e0124d7f05f35389
VladimirB312 <158708298+VladimirB312@users.noreply.github.com>
2024-05-22 00:46:50 +0300

    laba 8 add part2

--- laba8/burger.js
+++ laba8/burger.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const menuButton = document.querySelector('.menu-icon');  
-  const menuList = document.querySelector('.header-nav__menu-list');
-  console.log(menuButton);
+  const menuButton = document.querySelector('.menu-icon');    
+  const menuList = document.querySelector('.header-nav__menu-list');  
 
   menuButton.addEventListener('click', function() {
     menuList.classList.toggle('active');
+    menuButton.classList.toggle('active');
+    document.body.classList.toggle('_lock');
   })
 
   window.addEventListener('resize', function() {
     menuList.classList.remove('active');
   })
+
+
 });
